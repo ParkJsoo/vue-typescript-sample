6d60f94bf8eb77a711a3cd0b20c80624bb43f919
Henry <61155410+ParkJsoo@users.noreply.github.com>
2022-03-21 03:52:39 +0900

    vuex 추가 및 eslint rules 수정
    vue add vuex

--- src/app/about/view/about.vue
+++ src/app/about/view/about.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    count : {{ str }}
   </div>
 </template>
+
+<script lang="ts">
+import {Component, Vue, Watch} from 'vue-property-decorator';
+
+@Component
+export default class About extends Vue {
+  str = '';
+
+  created() {
+    this.$store.dispatch('increment');
+  }
+
+  @Watch('$store.state.count')
+  getCount() {
+    this.str = this.$store.getters.getCount;
+  }
+}
+</script>
---
+++ src/store/index.ts
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    count: 0
+  },
+  getters: {
+    getCount(state) {
+      return state.count.toString();
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  },
+  actions: {
+    increment({ commit }) {
+      commit('increment');
+    }
+  },
+  modules: {
+  }
+});
